46bbb8c65e575e47b93615a8451db5a659966829
Anaurumashvili1 <anaurumashvili@gmailcom>
2021-12-23 13:21:43 +0400

    style

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
     <>
       <div className="App">
         <Header />
-        <InnerPage />
-        <Checkout />
+        {/* <InnerPage /> */}
         <Footer />
       </div>
     </>
